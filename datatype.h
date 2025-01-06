3375995b8bd4a4407422f2ad4f02e341bbf2cd17
Tạ Xuân Chiến; HN-JPN-KS24C; lập trình scratch; session 3 <xuanchien319@gmail.com>
2025-01-06 09:58:44 +0700

    Add files via upload

---
+++ datatype.h
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+struct Book {
+	int id;
+	char name[100];
+	char author[100];
+	int quantity;
+	int price;
+	int publicationDate ;
+	int publicationMonth;
+	int publicationYear;
+};
+
+struct Costum{
+	int id;
+	char name[100];
+	char email[100];
+	char phone[20];
+};
+
